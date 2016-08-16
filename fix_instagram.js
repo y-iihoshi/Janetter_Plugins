c0c960f10d19c512890401ab8cf20d27102747d2
IIHOSHI Yoshinori <y-iihoshi@colorless-sight.jp>
2016-08-17 01:16:34 +0900

    fix_instagram.js ver. 0.1.0

---
+++ fix_instagram.js
@@ -0,0 +1,39 @@
+(function ($, jn) {
+
+	var my_filename = 'fix_instagram.js';
+
+	if ((_Janetter_Window_Type != "main") && (_Janetter_Window_Type != "profile"))
+		return;
+
+	function initOnInitialized() {
+		// Thanks to: https://y-dash.blogspot.jp/2016/03/janetterinstagram.html
+		var orig_janetterThumbnail = $.fn.janetterThumbnail.toString();
+		var re_instagram_exp = /^(.+\/https\?:\\\/\\\/)(\(instagr.+\/g,)$/m;
+
+		if (re_instagram_exp.test(orig_janetterThumbnail)) {
+			var replaced = orig_janetterThumbnail
+				.replace(re_instagram_exp,
+				         "$1(?:www\.)$2  // replaced by " + my_filename)
+			// console.log(replaced);
+			eval('$.fn.janetterThumbnail = ' + replaced);
+		} else {
+			new jn.msgdialog({
+				title: my_filename,
+				icon: '',
+				message: 'Sorry, ' + my_filename+ ' cannot be installed.',
+				buttons: [ janet.msg.ok ],
+			});
+		}
+	}
+
+	if (jn.temp.initialized) {
+		initOnInitialized();
+	} else {
+		var orig_onInitDone = jn.onInitializeDone;
+		jn.onInitializeDone = function() {
+			orig_onInitDone && orig_onInitDone.apply(this, arguments);
+			initOnInitialized();
+		};
+	}
+
+})(jQuery, janet);
