3609aabd94419e0cda98ff4c0161988d5f852476
IIHOSHI Yoshinori <y-iihoshi@colorless-sight.jp>
2016-09-22 22:39:30 +0900

    expand_tweet_url_mod.js v160922

--- expand_tweet_url_mod.js
+++ expand_tweet_url_mod.js
@@ -1,69 +1,73 @@
 //================================================
+// expand_tweet_url_mod.js
+// Author: @iihoshi
+//
+// This script is a derivative work of:
 // ツイートURLを展開
 // Author: @hina_016
 //================================================
 
 (function($, jn) {
-	
+
 	if(_Janetter_Window_Type != "main" && _Janetter_Window_Type != "profile") return;
-	
+
 	// プラグイン情報 ここから
 	// プラグイン情報の初期化
 	if(!jn.pluginInfo)
 		jn.pluginInfo = {};
 	// プラグイン情報本体
-	jn.pluginInfo['expand_tweet_url'] = {
+	jn.pluginInfo['expand_tweet_url_mod'] = {
 		'name' : {
 			'ja' : 'ツイートURLを展開',
 			'en' : 'Expand Tweet URL'
 		},
 		'author' : {
-			'ja' : '@hina_016',
-			'en' : '@hina_016'
+			'ja' : '@iihoshi',
+			'en' : '@iihoshi'
 		},
-		'version' : '130129',
-		'file' : 'expand_tweet_url.js',
-		'language' : ['ja'],
-		'last_update' : "2013/1/29",
+		'version' : '160922',
+		'file' : 'expand_tweet_url_mod.js',
+		'language' : ['en', 'ja'],
+		'last_update' : "2016/9/22",
 		'update_timezone' : '9',
-		'jnVersion' : '4.2.0.0',
+		'jnVersion' : '4.3.1.0',
 		'description' : {
 			'ja' : 'ツイートURLを展開して表示します。',
 			'en' : 'Expands tweet URLs and displays the contents.'
 		},
-		'updateinfo' : 'http://dl.dropbox.com/u/37073935/Janetter2/update_info.txt'
+		'updateinfo' : 'http://www.colorless-sight.jp/archives/JanetterPluginUpdateInfo.txt'
 	};
 	// プラグイン情報ここまで
-	
+
 	var init = jn.onInitializeDone;
 	jn.onInitializeDone = function() {
 		init && init();
 		main();
 	}
-	
+
 	var tweet_cache = {}; // id -> data
 	var tweet_re = /twitter.com\/.+\/status(es)?\/(\d+)(\/|\?.*)?$/;
-	
+
 	function main() {
 		// ツイートのURLをマウスオーバーした時
 		var target = _Janetter_Window_Type == "main" ? $("#timeline-view") : $("#prof-main");
 		target.on("mouseover", ".link", function(event) {
 			var elm = $(this);
 			var url = elm.attr("expanded");
-			
+
 			var id = url2id(url);
 			if(!id) return;
-			
+
 			show_quote(elm, id);
 		});
 	}
-	
+
 	function show_quote(elm, id) {
 		// box作成
 		var tweet = elm.closest(".tweet-reply");
 		if(tweet.length) {
 			var url_quote_box = tweet.next(".url-quote-box");
-			
+
 			if(!url_quote_box.length)
 				url_quote_box = $("<div class='url-quote-box'>").insertAfter(tweet);
 		} else {
@@ -73,30 +77,42 @@
 			if(!url_quote_box.length)
 				url_quote_box = $("<div class='url-quote-box'>").insertBefore(tweet_box.children(".tweet-reply-box"));
 		}
-		
+
 		// 展開済みならやめる
 		if(url_quote_box.data("etu_list" + id)) return;
-		
+
 		url_quote_box.data("etu_list" + id, true);
-		
+
+		var juid = _Janetter_Window_Type == "main"
+			? elm.closest(".timeline-container").attr("juid")
+			: jn.profileDialog.account.juid;
+
 		// boxに追加
-		$.when(get_tweet(id)).done(function(data) {
+		$.when(get_tweet(id, juid)).done(function(data) {
 			add_quote(url_quote_box, generate_quote(data, $(elm).attr("expanded"), id), tweet);
 		}).fail(function() {
 			jn.notice("ツイート展開に失敗しました。ID: " + id, 3000);
 		});
 	}
-	
-	function get_tweet(id) {
+
+	function get_tweet(id, juid) {
 		var dfd = $.Deferred();
-		
+
 		return tweet_cache[id] || (function() {
 			if(_Janetter_Window_Type == "main")
 				jn.notice("ツイート展開中… ID: " + id, 3000);
-			
+
 			jn.websocket.send({
-				action: 'statuses_show',
-				data: {id: id},
+				action: 'direct',
+				data: {
+					juid: juid,
+					method: 'GET',
+					path: '/statuses/show',
+					id: id,
+					include_entities: true,
+					api11: true,
+					tweet_mode: 'extended'
+				},
 				done: function(success, data, code) {
 					success ? dfd.resolve(data) : dfd.reject();
 				}
@@ -104,25 +120,30 @@
 			return dfd.promise();
 		})();
 	}
-	
+
 	function generate_quote(data, url, id) {
 		// dataのクローン作成
 		var data2 = JSON.parse(JSON.stringify(data));
-		
+
+		if(data2.full_text) {
+			data2.text = data2.full_text;
+			delete data2.full_text;
+		}
+
 		// replyのテンプレを使う
 		return jn.generateReply(data2).prepend("<span>Tweet ID: <a href='%1'>%2</a></span>".format(url, id)).data("etu_id", id);
 	}
-	
+
 	function add_quote(box, quote, tweet) {
 			var ch = box.children();
 			var link = tweet.find(".link");
-			
+
 			if(ch.length) {
 				var target_idx = 0;
 				link.each(function() {
 					var id = url2id($(this).attr("expanded"));
 					if(!id) return;
-					
+
 					if(id == quote.data("etu_id")) {
 						ch.eq(target_idx).before(quote);
 						return false;
@@ -137,13 +158,13 @@
 			} else
 				box.append(quote);
 	}
-	
+
 	function url2id(url) {
 		if(!url) return;
-		
+
 		var match = url.match(tweet_re);
 		if(!match) return;
-		
+
 		return match[2];
 	}
 
