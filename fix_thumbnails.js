846acb3f62fa7ffeebc51f9caadb2d5a3192ed0d
IIHOSHI Yoshinori <y-iihoshi@colorless-sight.jp>
2016-10-03 03:20:00 +0900

    fix_thumbnails.js: Support for pixiv

--- fix_thumbnails.js
+++ fix_thumbnails.js
@@ -15,14 +15,37 @@
 			: original;
 	}
 
+	// Thanks to: http://potato.2ch.net/test/read.cgi/software/1438689601/763
+	function fixPixiv() {
+		var original = this.toString();
+		var re_pixiv = /function pixiv\(permalink\){[^]*?^\t\t}$/m;
+		if (re_pixiv.test(original)) {
+			var pixiv = re_pixiv.exec(original)[0];
+			var re_pixiv_cond = /if\(link && thumb_url && title\){$/m;
+			var re_pixiv_link = /link: link\[1\]\.htmlDecode\(\),$/m;
+			if (re_pixiv_cond.test(pixiv) && re_pixiv_link.test(pixiv)) {
+				var replaced_pixiv = pixiv 
+					.replace(re_pixiv_cond, "if(thumb_url && title){" + comment)
+					.replace(re_pixiv_link, "link: url," + comment);
+				return original.replace(re_pixiv, replaced_pixiv);
+			} else {
+				return original;
+			}
+		} else {
+			return original;
+		}
+	}
+
 	function initOnInitialized() {
 		String.prototype.fixInstagram = fixInstagram;
+		String.prototype.fixPixiv = fixPixiv;
 
 		var orig_janetterThumbnail = $.fn.janetterThumbnail.toString();
-		var replaced = orig_janetterThumbnail.fixInstagram();
+		var replaced = orig_janetterThumbnail.fixInstagram().fixPixiv();
 		// console.log(replaced);
 		eval('$.fn.janetterThumbnail = ' + replaced);
 
+		delete String.prototype.fixPixiv;
 		delete String.prototype.fixInstagram;
 	}
 
