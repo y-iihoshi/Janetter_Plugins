84dff68a860af8dae3ec179b095e967c7c6b52ca
IIHOSHI Yoshinori <y-iihoshi@colorless-sight.jp>
2016-10-03 03:18:19 +0900

    fix_thumbnails.js: Refactor codes

--- fix_thumbnails.js
+++ fix_thumbnails.js
@@ -1,29 +1,29 @@
 (function ($, jn) {
 
-	var my_filename = 'fix_thumbnails.js';
-
 	if ((_Janetter_Window_Type != "main") && (_Janetter_Window_Type != "profile"))
 		return;
 
-	function initOnInitialized() {
-		// Thanks to: https://y-dash.blogspot.jp/2016/03/janetterinstagram.html
-		var orig_janetterThumbnail = $.fn.janetterThumbnail.toString();
-		var re_instagram_exp = /^(.+\/https\?:\\\/\\\/)(\(instagr.+\/g,)$/m;
+	var my_filename = 'fix_thumbnails.js';
+	var comment = '  // replaced by ' + my_filename;
 
-		if (re_instagram_exp.test(orig_janetterThumbnail)) {
-			var replaced = orig_janetterThumbnail
-				.replace(re_instagram_exp,
-				         "$1(?:www\.)$2  // replaced by " + my_filename);
-			// console.log(replaced);
-			eval('$.fn.janetterThumbnail = ' + replaced);
-		} else {
-			new jn.msgdialog({
-				title: my_filename,
-				icon: '',
-				message: 'Sorry, ' + my_filename+ ' cannot be installed.',
-				buttons: [ janet.msg.ok ],
-			});
-		}
+	// Thanks to: https://y-dash.blogspot.jp/2016/03/janetterinstagram.html
+	function fixInstagram() {
+		var original = this.toString();
+		var re_instagram_exp = /^(.+\/https\?:\\\/\\\/)(\(instagr.+\/g,)$/m;
+		return re_instagram_exp.test(original)
+			? original.replace(re_instagram_exp, "$1(?:www\.)$2" + comment)
+			: original;
+	}
+
+	function initOnInitialized() {
+		String.prototype.fixInstagram = fixInstagram;
+
+		var orig_janetterThumbnail = $.fn.janetterThumbnail.toString();
+		var replaced = orig_janetterThumbnail.fixInstagram();
+		// console.log(replaced);
+		eval('$.fn.janetterThumbnail = ' + replaced);
+
+		delete String.prototype.fixInstagram;
 	}
 
 	if (jn.temp.initialized) {
